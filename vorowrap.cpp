e2c76b089766c252c74d4c9b331540fc965274a2
Jimmy <zaphos@gmail.com>
2016-04-08 00:53:30 -0700

    sketching out potential voronoi class structure

--- vorowrap.cpp
+++ vorowrap.cpp
@@ -143,17 +143,128 @@ int main() {
     emscripten_run_script("ready_for_emscripten_calls = true;");
 }
 
+// indices to connect cell to voro++ container
+struct CellConLink {
+    CellConLink() : ijk(-1), q(-1) {} // invalid / fail-fast defaults
+    int ijk;    // block index in voro++ container
+    int q;      // index of the cell within the block (following var naming from c_loops.hh)
+};
+
+// defining information about cell
+struct Cell {
+    Cell() {}
+    Cell(int id, int type, glm::dvec3 pos) : id(id), type(type), pos(pos) {}
+    int id; // id of the cell; e.g. what is return in neighbor queries.  By convention, should be kept as index of cell in cell vector.
+    int type; // 0 is empty, non-zero is non-empty; can use different numbers as tags or material types
+    glm::dvec3 pos;
+    CellConLink link;
+};
+
+struct CellCache { // computations from a voro++ computed cell
+    vector<int> faces; // faces as voro++ likes to store them -- packed as [#vs in f0, f0 v0, f0 v1, ..., #vs in f1, ...]
+    vector<double> vertices; // vertex coordinates, indexed by faces array
+    vector<int> neighbors; // cells neighboring each face
+    
+    void clear() { faces.clear(); vertices.clear(); neighbors.clear(); }
+    void create(const glm::dvec3 &pos, voro::voronoicell_neighbor &c) {
+        c.neighbors(neighbors);
+        // fills facev w/ faces as (#verts in face 1, face vert ind 1, ind 2, ..., #vs in f 2, f v ind 1, etc)
+        c.face_vertices(faces);
+        // makes all the vertices for the faces to reference
+        c.vertices(pos.x, pos.y, pos.z, vertices);
+    }
+};
+
+struct CellGLBuffers {
+    vector<float> vertices;
+    vector<float> normals;
+    
+    float* getBuffer() { return &vertices[0]; }
+    int getLen() { return vertices.size(); }
+};
+
 struct Voro {
-    Voro() {}
-    Voro(glm::vec3 low, glm::vec3 high) : b_min(low), b_max(high) {}
+    Voro() : b_min(glm::vec3(-10)), b_max(glm::vec3(10)), con(0) {}
+    Voro(glm::vec3 bound_min, glm::vec3 bound_max) : b_min(bound_min), b_max(bound_max), con(0) {}
+    ~Voro() {
+        delete con;
+    }
     
     union { // bounding box range
         struct {glm::vec3 b_min, b_max;};
         glm::vec3 bounds[2];
     };
+    voro::container *con;
+    vector<Cell> cells;
+    vector<CellCache> cell_caches;
+    vector<CellGLBuffers> cell_buffers;
     
-    void hi() { cout << "hi" << endl; }
-    // etc
+    void buffersFromCellCache(int cell, const CellCache &cache, CellGLBuffers &buffers, int mode) {
+        
+    }
+    
+    void create(float *points, int *types, int numPts, bool computeCaches, bool computeBuffers) {
+        // Put all the points in a pre_container
+        voro::pre_container pcon(b_min.x,b_max.x,b_min.y,b_max.y,b_min.z,b_max.z,false,false,false);
+        float *pt = points;
+        for (int i=0; i<numPts; i++, pt+=3) {
+            pcon.put(i++,pt[0],pt[1],pt[2]);
+            cells.push_back(Cell(i, types[i], glm::vec3(pt[0],pt[1],pt[2])));
+        }
+        
+        // Set up the number of blocks that the container is divided into
+        int n_x, n_y, n_z;
+        pcon.guess_optimal(n_x,n_y,n_z);
+        
+        // Set up the container class and import the particles from the pre-container
+        con = new voro::container(pcon.ax,pcon.bx,pcon.ay,pcon.by,pcon.az,pcon.bz,n_x,n_y,n_z,false,false,false,10);
+        pcon.setup(*con);
+        
+        voro::voronoicell_neighbor c;
+        voro::c_loop_all vl(*con);
+        CellCache cache;
+//        int count = 0, tmpcount = 0;
+//        int testneigh = -1, teststart = -1;
+//        int output_i = 0;
+//        if(vl.start()) do if(con.compute_cell(c,vl)) {
+//            
+//            
+//            double *pp = con.p[vl.ijk]+con.ps*vl.q;
+//            
+//            int myid = con.id[vl.ijk][vl.q];
+//            
+//            // todo remove -- for testing, just skip most of the cells (so we don't end up with a single solid block)
+//            if (rand() % 5 != 1) {
+//                continue;
+//            }
+//            
+//            
+//            c.neighbors(neighbors);
+//            // fills facev w/ faces as (#verts in face 1, face vert ind 1, ind 2, ..., #vs in f 2, f v ind 1, etc)
+//            c.face_vertices(facev);
+//            // makes all the vertices for the faces to reference
+//            c.vertices(pp[0], pp[1], pp[2], vcoords);
+//            
+//            for (int i = 0, ni = 0; i < (int)facev.size(); i+=facev[i]+1, ni++) {
+//                //            if (myid > neighbors[ni])
+//                {
+//                    //                connect(myid, neighbors[ni]);
+//                    // triangulate each face of the voronoi diagram
+//                    int vicount = (i+facev[i]+1)-(i+1);
+//                    int firstv = facev[i+1];
+//                    int prevv = facev[i+2];
+//                    for (int j = i+3; j < i+facev[i]+1; j++) { // facev
+//                        int nextv = facev[j];
+//                        output_vert_and_incr(output, output_i, vcoords, firstv, max_pts_output);
+//                        output_vert_and_incr(output, output_i, vcoords, nextv, max_pts_output);
+//                        output_vert_and_incr(output, output_i, vcoords, prevv, max_pts_output);
+//                        prevv = nextv;
+//                    }
+//                }
+//            }
+//        } while(vl.inc());
+
+    }
 };
 
 EMSCRIPTEN_BINDINGS(voro) {
@@ -166,7 +277,6 @@ EMSCRIPTEN_BINDINGS(voro) {
     .constructor<glm::vec3, glm::vec3>()
     .property("min", &Voro::b_min)
     .property("max", &Voro::b_max)
-    .function("hi", &Voro::hi)
     ;
 
 }
